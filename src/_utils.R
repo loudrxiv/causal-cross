bdc74b2c397089329c190b039366fa0492c72037
mebeid1 <mae117@pitt.edu>
2023-08-24 10:09:10 -0400

    getting ready to migrate to bitbucket

--- src/_utils.R
+++ src/_utils.R
@@ -24,113 +24,59 @@ tfpr  <- reticulate::import("tensorflow_probability")
 tfp   <- tfpr$distributions
 tfb   <- tfpr$bijectors
 
-#----- Set up some 'constants'
-
-BATCH_SIZE <- 2**9
-VALBATCH_SIZE <- 2**9
-
-HOCOMOCO = "/net/talisker/home/benos/mae117/Documents/research/dennis/causal-domain-adaptation/data/hocomoco/human/hoc_pwms_hg_16.rdat"
-
-#" General Sets
-SPECIES = list("mm10", "hg38")
-TFS = list("CTCF", "CEBPA", "Hnf4a", "RXRA")
-
-GENOMES = list("mm10"="/net/talisker/home/benos/mae117/Documents/research/dennis/causal-domain-adaptation/data/mm10/mm10_no_alt_analysis_set_ENCODE.fasta",
-  "hg38"="/net/talisker/home/benos/mae117/Documents/research/dennis/causal-domain-adaptation/data/hg38/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta")
+#----- Set up some variables & 'constants'
 
 ROOT = "/net/talisker/home/benos/mae117/Documents/research/dennis/causal-domain-adaptation"
-DATA_ROOT = paste0(ROOT,"/data/")
-FIGURE_ROOT = paste0(ROOT,"/figures/")
-MODEL_ROOT = paste0(ROOT,"/models/")
+TFS       <- c("CTCF")
+SPECIES   <- c("mm10", "hg38")
+RUNS      <- c(1)
 
-#" These files are created by the script 1_make_training_and_testing_data/1_runall_setup_model_data.sh
-VAL_FILENAME = "chr1_random_1m.bed"
-TRAIN_POS_FILENAME = "chr3toY_pos_shuf.bed"
-TRAIN_NEG_FILENAME = "chr3toY_neg_shuf_run1_1E.bed"
-TEST_FILENAME = "chr2.bed"
+# data files generated from shaun's scripts
+VAL_FILENAME        <- "chr1_random_1m.bed"
+TEST_FILENAME       <- "chr2.bed"
+TRAIN_POS_FILENAME  <- "chr3toY_pos_shuf.bed"
+TRAIN_NEG_FILENAME  <- "chr3toY_neg_shuf_runX_ZE.bed"
 
-#" Where models will be saved during/after training
-MODEL_ROOT = paste0(ROOT,"/models/")
-DA_ILCC_ROOT = paste0(MODEL_ROOT, "da_ilcc/")
-VAE_ILCC_ROOT = paste0(MODEL_ROOT, "vae_ilcc/")
-KL_ILCC_ROOT = paste0(MODEL_ROOT, "kl_ilcc/")
-TIMESTAMP_FORMAT = "%Y-%m-%d_%H-%M-%S"
-
-base_params = list("batchsize" = 400,  # number of examples seen every batch during training
-                "seqlen" = 500,       # the input sequence length that will be expected by the model
-                "convfilters" = 240,   # number of filters in the convolutional layer
-                "filtersize" = 20,     # the size of the convolutional filters
-                "strides" = 15,        # the max-pooling layer's stride
-                "pool_size" = 15,      # the max-pooling layer's pooling size
-                "lstmnodes" = 32,      # "width" of the LSTM layer
-                "dl1nodes" = 1024,     # neurons in the first dense layer (after LSTM)
-                "dl2nodes" = 512,      # neurons in the second dense layer (before output)
-                "dropout" = 0.5,       # fraction of neurons in the first dense layer to randomly dropout
-                "valbatchsize" = 10000,
-                "epochs" = 200)
-
-
-ilcc_params = list("batchsize" = 400,  # number of examples seen every batch during training
-                "seqlen" = 500,       # the input sequence length that will be expected by the model
-                "convfilters" = 240,   # number of filters in the convolutional layer
-                "filtersize" = 20,     # the size of the convolutional filters
-                "strides" = 15,        # the max-pooling layer's stride
-                "pool_size" = 15,      # the max-pooling layer's pooling size
-                "lstmnodes" = 32,      # "width" of the LSTM layer
-                "dl1nodes" = 1024,     # neurons in the first dense layer (after LSTM)
-                "dl2nodes" = 512,      # neurons in the second dense layer (before output)
-                "dropout" = 0.5,       # fraction of neurons in the first dense layer to randomly dropout
-                "valbatchsize" = 10000,
-                "epochs" = 15)
-
-#" Set changing variables
-tfs            = TFS[4]
-
-source_species = SPECIES[1]
-target_species = SPECIES[2]
-
-source_root = paste0(DATA_ROOT, source_species, "/", tfs, "/")
-target_root = paste0(DATA_ROOT, target_species, "/", tfs, "/")
-
-src_bindingtrainposfile = paste0(source_root, TRAIN_POS_FILENAME)
-src_bindingtrainnegfile = paste0(source_root, TRAIN_NEG_FILENAME)
-tar_bindingtrainposfile = paste0(target_root, TRAIN_POS_FILENAME)
-tar_bindingtrainnegfile = paste0(target_root, TRAIN_NEG_FILENAME)
-
-sourcevalfile = paste0(source_root, VAL_FILENAME)
-targetvalfile = paste0(target_root, VAL_FILENAME)
-
-sourcetestfile = paste0(source_root, TEST_FILENAME)
-targettestfile = paste0(target_root, TEST_FILENAME)
+params <- list(
+  "batchsize" = 512,       # number of examples seen every batch during training
+  #"seqlen" = 500,         # the input sequence length that will be expected by the model
+  #"convfilters" = 240,    # number of filters in the convolutional layer
+  #"filtersize" = 20,      # the size of the convolutional filters
+  #"strides" = 15,         # the max-pooling layer's stride
+  #"pool_size" = 15,       # the max-pooling layer's pooling size
+  #"lstmnodes" = 32,       # "width" of the LSTM layer
+  #"dl1nodes" = 1024,      # neurons in the first dense layer (after LSTM)
+  #"dl2nodes" = 512,       # neurons in the second dense layer (before output)
+  #"dropout" = 0.5,        # fraction of neurons in the first dense layer to randomly dropout
+  "valbatchsize" = 10000   # Talking about the testing data
+  #"epochs" = 15
+)
 
 #----- Functions
 
 # Read in the PWSM
-gen_datamotifs <- function(path = "/net/talisker/home/benos/mae117/Documents/research/dennis/causal-domain-adaptation/data", safety = FALSE, save = FALSE) {
-        
-    pwm_filters <- readRDS(HOCOMOCO) |> lapply(as.matrix)                                                                                                                                                                             
+gen_datamotifs <- function(path = ROOT, safety = FALSE, save = FALSE) {
+
+  hocomoco_loc      <- paste0(path, "/data/hocomoco/human/hoc_pwms_hg_16.rdat")
+  hocomoco_filters  <- readRDS(hocomoco_loc) |> lapply(as.matrix)
+
+  if (safety == TRUE) {
+
+      message("Checking the size of the PWM...\n")
+
+      if ( any(lapply(hocomoco_filters, is.matrix) |> unlist() == FALSE) ) { stop("filters invalid\n") }
+      if ( any(lapply(hocomoco_filters, ncol) |> unlist() != ncol(hocomoco_filters[[1]])) ) { stop("filters invalid\n") }
+      if ( any(lapply(hocomoco_filters, nrow) |> unlist() != nrow(hocomoco_filters[[1]])) ) { stop("filters invalid\n") }
+
+  }
                                                                                                                                                                                                                                         
-    if( safety == TRUE) {
-
-        message("Checking the size of the PWM")
-
-        if(any(lapply(filters, is.matrix) |> unlist() == FALSE))            
-        stop("filters invalid\n")
-
-        if(any(lapply(filters, ncol) |> unlist() != ncol(filters[[1]]))) 
-        stop("filters invalid\n")
-
-        if(any(lapply(filters, nrow) |> unlist() != nrow(filters[[1]]))) 
-        stop("filters invalid\n") 
-    }
-                                                                                                                                                                                                                                        
-    filter_tensor <- pwm_filters |>                                                                                                                                                                                                   
+    filter_tensor <- hocomoco_filters |>                                                                                                                                                                                                   
     unlist() |>                                                                                                                                                                                                                          
-    array(dim = c(nrow(pwm_filters[[1]]),4,length(pwm_filters)))
+    array(dim = c(nrow(hocomoco_filters[[1]]),4,length(hocomoco_filters)))
 
-    if( save == TRUE ) {
+    if ( save == TRUE ) {
         message("Saving motif data to data/PWM.RData")
-        save(filter_tensor, file = glue(path, "/PWM.RData"))
+        save(filter_tensor, file = paste0(path, "/PWM.RData"))
     }
 
     return(filter_tensor)
@@ -139,54 +85,77 @@ gen_datamotifs <- function(path = "/net/talisker/home/benos/mae117/Documents/res
 
 # Generate and offer to save the training data
 
-gen_datatrain <- function(path = "/net/talisker/home/benos/mae117/Documents/research/dennis/causal-domain-adaptation/data", save = FALSE) {
+# so essentially what I want to do is go through this step by step: 
+# 1. there is 1 pos train file per species (272661 seqs) I want to combine the 15 negative examples (total 272661) into a block and then combine those two toghethe
+# 2. do this for the other species and create the labels
+# 3. the validation data requires no effort, but make sure the species labels are correct
+# 4. chr2 is the test and the 'validation' data we use here
 
-    X_gr_src = GRangesList("src_bindingtrainpos"=import(src_bindingtrainposfile),                                                                                                                                                   
-                        "src_bindingtrainneg"=import(src_bindingtrainnegfile),
-                        "src_bindingtest"=import(sourcetestfile),
-                        "src_bindingval"=import(sourcevalfile))
+gen_datatrain <- function(path = ROOT, save = FALSE) {
 
-    X_gr_tar = GRangesList("tar_bindingtrainpos"=import(tar_bindingtrainposfile),                                                                                                                                                   
-                        "tar_bindingtrainneg"=import(tar_bindingtrainnegfile),
-                        "tar_bindingtest"=import(targettestfile),
-                        "tar_bindingval"=import(targetvalfile))
+    # Create the files names for the negative bound sequences
+    construct_names <- function(root = ROOT, tfs = TFs, runs = RUNS) {
+      names <- c()
+      for (tf in tfs) {
+        for (spec in species) {
+          for (run in runs) {
+            names <- c(names, paste0(tf, "_", spec, "_run", run))
+          }
+        }
+      }
 
-    X_data_src      = lapply(X_gr_src, function(x) Biostrings::getSeq(Mmusculus, x))                                                                                                                                                       
-    X_labels_src    = lapply(X_gr_src, function(x) mcols(x)$name)                                                                                                                                                                          
-    D_labels_src    = lapply(X_gr_src, function(x) rep(1, length(x)))
-                            
-    X_data_tar      = lapply(X_gr_tar, function(x) Biostrings::getSeq(Hsapiens, x))                                                                                                                                                        
-    X_labels_tar    = lapply(X_gr_tar, function(x) mcols(x)$name)                                                                                                                                                                          
-    D_labels_tar    = lapply(X_gr_tar, function(x) rep(0, length(x)))
-                            
-    X_train = c(X_data_src$src_bindingtrainpos,
-                X_data_src$src_bindingtrainneg,
-                X_data_tar$tar_bindingtrainpos,
-                X_data_tar$tar_bindingtrainneg)
+      return(names)
 
-    y_trainbinding  = c(X_labels_src$src_bindingtrainpos, 
-                        X_labels_src$src_bindingtrainneg,
-                        X_labels_tar$tar_bindingtrainpos,
-                        X_labels_tar$tar_bindingtrainneg)
-
-    y_traindomain   = c(D_labels_src$src_bindingtrainpos,
-                        D_labels_src$src_bindingtrainneg,
-                        D_labels_tar$tar_bindingtrainpos,
-                        D_labels_tar$tar_bindingtrainneg)
-
-    nNs       = Biostrings::vcountPattern('N', X_train)
-    xwidth    = width(X_train)
-
-    X_train        = X_train[xwidth==500][nNs==0] |> make_ints()
-    y_traindomain  = y_traindomain[xwidth==500][nNs==0]
-    y_trainbinding = y_trainbinding[xwidth==500][nNs==0]
-
-    if( save == TRUE ) {
-        message("Saving training data to data/train.RData")
-        save(X_train, y_trainbinding, y_traindomain, file = glue(path, "/train.RData"))
     }
+
+     rep(paste0(ROOT, "/data/"), 15)
+
+    # X_gr_src = GRangesList("src_bindingtrainpos"=import(src_bindingtrainposfile),                                                                                                                                                   
+    #                     "src_bindingtrainneg"=import(src_bindingtrainnegfile),
+    #                     "src_bindingtest"=import(sourcetestfile),
+    #                     "src_bindingval"=import(sourcevalfile))
+
+    # X_gr_tar = GRangesList("tar_bindingtrainpos"=import(tar_bindingtrainposfile),                                                                                                                                                   
+    #                     "tar_bindingtrainneg"=import(tar_bindingtrainnegfile),
+    #                     "tar_bindingtest"=import(targettestfile),
+    #                     "tar_bindingval"=import(targetvalfile))
+
+    # X_data_src      = lapply(X_gr_src, function(x) Biostrings::getSeq(Mmusculus, x))                                                                                                                                                       
+    # X_labels_src    = lapply(X_gr_src, function(x) mcols(x)$name)                                                                                                                                                                          
+    # D_labels_src    = lapply(X_gr_src, function(x) rep(1, length(x)))
+                            
+    # X_data_tar      = lapply(X_gr_tar, function(x) Biostrings::getSeq(Hsapiens, x))                                                                                                                                                        
+    # X_labels_tar    = lapply(X_gr_tar, function(x) mcols(x)$name)                                                                                                                                                                          
+    # D_labels_tar    = lapply(X_gr_tar, function(x) rep(0, length(x)))
+                            
+    # X_train = c(X_data_src$src_bindingtrainpos,
+    #             X_data_src$src_bindingtrainneg,
+    #             X_data_tar$tar_bindingtrainpos,
+    #             X_data_tar$tar_bindingtrainneg)
+
+    # y_trainbinding  = c(X_labels_src$src_bindingtrainpos, 
+    #                     X_labels_src$src_bindingtrainneg,
+    #                     X_labels_tar$tar_bindingtrainpos,
+    #                     X_labels_tar$tar_bindingtrainneg)
+
+    # y_traindomain   = c(D_labels_src$src_bindingtrainpos,
+    #                     D_labels_src$src_bindingtrainneg,
+    #                     D_labels_tar$tar_bindingtrainpos,
+    #                     D_labels_tar$tar_bindingtrainneg)
+
+    # nNs       = Biostrings::vcountPattern('N', X_train)
+    # xwidth    = width(X_train)
+
+    # X_train        = X_train[xwidth==500][nNs==0] |> make_ints()
+    # y_traindomain  = y_traindomain[xwidth==500][nNs==0]
+    # y_trainbinding = y_trainbinding[xwidth==500][nNs==0]
+
+    # if( save == TRUE ) {
+    #     message("Saving training data to data/train.RData")
+    #     save(X_train, y_trainbinding, y_traindomain, file = glue(path, "/train.RData"))
+    # }
     
-    return( c(X_train, y_traindomain, y_trainbinding) )
+    # return( c(X_train, y_traindomain, y_trainbinding) )
 
 }
 
